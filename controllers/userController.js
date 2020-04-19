92b77823cb852d9b9d14540be1fd99d1bdf1dbee
Gustavo <gustavo.maes@fullbar.com.br>
2020-04-18 23:59:44 -0300

    base user controller

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).send({ message: 'Erro ao buscar os usuários' })
+}
+
+exports.createUser = (req, res) => {
+    res.status(500).send({ message: 'Erro ao buscar os usuários' })
+}
+
+exports.getUser = (req, res) => {
+    res.status(500).send({ message: 'Erro ao buscar os usuários' })
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).send({ message: 'Erro ao buscar os usuários' })
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).send({ message: 'Erro ao buscar os usuários' })
+}

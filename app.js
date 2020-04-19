1d43be4cfad7b742d4879ec9c1b9d2507ec39688
Gustavo <gustavo.maes@fullbar.com.br>
2020-04-18 23:59:37 -0300

    created server.js and setting routes and controllers in app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 
@@ -8,8 +11,6 @@ app.use(bodyParser.json({
     limit: '10mb'
 }))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api/v1/users', userRoutes)
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+module.exports = app 

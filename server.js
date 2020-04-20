e8db27a31ef84c3d45fb41b932cd57e56c259722
Gustavo <gustavo.maes@fullbar.com.br>
2020-04-19 23:26:42 -0300

    connect server on mongodb using mongoose

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB Connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

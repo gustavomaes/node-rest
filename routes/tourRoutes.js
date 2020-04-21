9d551a1bbec27dd363aae2f5a8955721c3dfaf41
Gustavo <gustavomaescd@gmail.com>
2020-04-21 01:14:17 -0300

    fix tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,10 +3,8 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
 router.get('/', tourController.getAllTours);
-router.post('/', tourController.checkBody, tourController.createTour);
+router.post('/', tourController.createTour);
 router.get('/:id', tourController.getTour);
 router.patch('/:id', tourController.updateTour);
 router.delete('/:id', tourController.deleteTour);

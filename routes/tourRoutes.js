34e05c80fdc8436a1d121835c30c552ff20402ae
Gustavo <gustavomaescd@gmail.com>
2020-04-23 20:53:01 -0300

    use middleware to create a filter alias

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,11 @@ const tourController = require('../controllers/tourController');
 const router = express.Router();
 
 router.get('/', tourController.getAllTours);
+router.get(
+    '/top-5-cheap',
+    tourController.aliasTopTours,
+    tourController.getAllTours
+);
 router.post('/', tourController.createTour);
 router.get('/:id', tourController.getTour);
 router.patch('/:id', tourController.updateTour);

a77c99d44de696e21865041cbf8b4ce0b12d93c6
Gustavo <gustavomaescd@gmail.com>
2020-05-20 22:12:25 -0300

    handle diferent erros to dev and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,35 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).send({
+        status: err.status,
+        error: err,
+        message: err.message,
+        tack: err.stack,
+    });
+};
+
+const sendErrorProduction = (err, res) => {
+    if (err.operational) {
+        res.status(err.statusCode).send({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        console.err('ERROR ', err);
+
+        res.status(500).send({
+            status: 'error',
+            message: 'Something ent very wrong',
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    res.status(err.statusCode).send({
-        status: err.status,
-        message: err.message,
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProduction(err, res);
+    }
 };

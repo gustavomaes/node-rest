8c5a6cdcc67eac98ef12fe975a026529ea72aa0c
Gustavo <gustavomaescd@gmail.com>
2020-06-01 23:32:50 -0300

    add handling cast error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,21 +1,26 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).send({
         status: err.status,
         error: err,
         message: err.message,
-        tack: err.stack,
+        stack: err.stack,
     });
 };
 
 const sendErrorProduction = (err, res) => {
-    if (err.operational) {
+    if (err.isOperational) {
         res.status(err.statusCode).send({
             status: err.status,
             message: err.message,
         });
     } else {
-        console.err('ERROR ', err);
-
         res.status(500).send({
             status: 'error',
             message: 'Something ent very wrong',
@@ -30,6 +35,9 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProduction(err, res);
+        let error = { ...err };
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+        sendErrorProduction(error, res);
     }
 };

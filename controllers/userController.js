c17998440a6bc5aa23715faf81f49ba633d38842
Gustavo <gustavomaescd@gmail.com>
2020-06-06 23:59:01 -0300

    create a basic getAllUsers route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,21 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).send({ message: 'Erro ao buscar os usuários' });
-};
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    // const feature = new APIFeatures(Tour.find(), req.query)
+    //     .filter()
+    //     .sort()
+    //     .limitFields()
+    //     .paginate();
+    const users = await User.find();
+    res.status(200).send({
+        status: 'success',
+        results: users.length,
+        data: {
+            users,
+        },
+    });
+});
 
 exports.createUser = (req, res) => {
     res.status(500).send({ message: 'Erro ao buscar os usuários' });
